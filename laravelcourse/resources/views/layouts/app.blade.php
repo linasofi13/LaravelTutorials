faf5cf3f6d4e346d8e5e8aecd0538e1ab423b079
Lina Ballesteros <lina.ballesteros13@gmail.com>
2024-08-18 23:52:06 -0500

    added products menu option

--- laravelcourse/resources/views/layouts/app.blade.php
+++ laravelcourse/resources/views/layouts/app.blade.php
@@ -21,6 +21,7 @@
       <a class="nav-link active" href="{{ route('home.index') }}">Home</a>
           <a class="nav-link active" href="{{ route('home.about') }}">About</a>
             <a class="nav-link active" href="{{ route('home.contact') }}">Contact</a>
+            <a class="nav-link active" href="{{ route('product.index') }}">Products</a>
         </div>
       </div>
     </div>
